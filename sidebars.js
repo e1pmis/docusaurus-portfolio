c88d89f29e261184aca5dc9c2aea09d2f7c6c9ee
Abdullah <alibrahimabdullah@gmx.de>
2025-09-20 22:07:08 +0000

    Add: sidebar script file

---
+++ sidebars.js
@@ -0,0 +1,41 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Projects',
+      collapsible: true,
+      collapsed: false,
+      items: [
+        'conduit-container/index',
+        {
+          type: 'category',
+          label: 'OWASP Juice Shop',
+          collapsible: true,
+          collapsed: false,
+          items: [
+            'owasp-juice-shop/index',
+            {
+              type: 'category',
+              label: 'Challenges',
+              collapsible: true,
+              collapsed: false,
+              items: [
+                'owasp-juice-shop/challenges/admin-registration',
+                'owasp-juice-shop/challenges/api-only-xss',
+                'owasp-juice-shop/challenges/captcha-bypass',
+                'owasp-juice-shop/challenges/forged-feedback',
+              ],
+            },
+          ],
+        },
+        'minecraft-server/index',
+        'word-press/index',
+        'truck-signs-api/index',
+        'baby-tools-shop/index',
+      ],
+    },
+  ],
+};
+
+module.exports = sidebars;
