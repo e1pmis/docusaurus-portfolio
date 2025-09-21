65db6e1ea17c03b1223fe185eeec5fc7f4725a1c
Abdullah <alibrahimabdullah@gmx.de>
2025-09-21 10:55:06 +0000

    edit: production URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://e1pmis.github.io",
+  url: "https://alibrahimabdullah.de",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
